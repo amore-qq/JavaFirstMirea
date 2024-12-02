fbca8e3323a351570c88c3915cc3d14abc355b90
amore-qq <tsuki_flame@mail.ru>
2024-12-02 22:26:56 +0300

    task 9

---
+++ src/ru/mirea/task9/Test.java
@@ -0,0 +1,60 @@
+package ru.mirea.task9;
+
+import java.util.Random;
+
+class Student {
+    private String name;
+    private int id;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
+
+public class Test {
+    public static void sort(Student[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            Student curr = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].getId() > curr.getId()) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = curr;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student[] students = new Student[20];
+        Random random = new Random();
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student(random.nextInt(500), String.format("Student №%d", i + 1));
+        }
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
+        sort(students);
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+}
